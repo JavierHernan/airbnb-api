9693d0b00fffe94af802032ce2ebef4cdf708961
Javier <javierLhernandez@yahoo.com>
2024-11-11 19:18:39 -0500

    added seeders/ in dev

--- backend/db/seeders/02-demo-spot.js
+++ backend/db/seeders/02-demo-spot.js
@@ -11,8 +11,8 @@ if (process.env.NODE_ENV === 'production') {
 module.exports = {
   async up (queryInterface, Sequelize) {
     await Spot.bulkCreate([
-      {
-        ownerId: 1,
+      { //spotId 1
+        ownerId: 1, 
         address: "1234 Fake Address Lane",
         city: "Fake City 1",
         state: "FakeState2",
@@ -24,7 +24,7 @@ module.exports = {
         price: "100",
         
       },
-      {
+      { //spotId 2
         ownerId: 1,
         address: "5678 Fake Address Lane",
         city: "Fake City 2",
@@ -36,7 +36,7 @@ module.exports = {
         description: "fake description 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         price: "100",
       },
-      {
+      { //spotId 3
         ownerId: 2,
         address: "5678999 Fake Address Lane",
         city: "Fake City 3",
@@ -48,7 +48,7 @@ module.exports = {
         description: "fake description 3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         price: "100",
       },
-      {
+      { //spotId 4
         ownerId: 2,
         address: "567899 Fake Address Lane",
         city: "Fake City 4",
@@ -60,6 +60,66 @@ module.exports = {
         description: "fake description 4 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         price: "100",
       },
+      { //spotId 5
+        ownerId: 1,
+        address: "1111 Fake Address Lane",
+        city: "Fake City 5",
+        state: "FakeState5",
+        country: "United States",
+        lat: 29.63933,
+        lng: -5.06108,
+        name: "Fake Name 5",
+        description: "fake description 5 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        price: "150",
+      },
+      { //spotId 6
+        ownerId: 3,
+        address: "2222 Fake Address Lane",
+        city: "Fake City 6",
+        state: "FakeState6",
+        country: "United States",
+        lat: 29.63933,
+        lng: -5.06108,
+        name: "Fake Name 6",
+        description: "fake description 6 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        price: "120",
+      },
+      { //spotId 7
+        ownerId: 2,
+        address: "3333 Fake Address Lane",
+        city: "Fake City 7",
+        state: "FakeState7",
+        country: "United States",
+        lat: 29.63933,
+        lng: -5.06108,
+        name: "Fake Name 7",
+        description: "fake description 7 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        price: "130",
+      },
+      { //spotId 8
+        ownerId: 3,
+        address: "4444 Fake Address Lane",
+        city: "Fake City 8",
+        state: "FakeState8",
+        country: "United States",
+        lat: 29.63933,
+        lng: -5.06108,
+        name: "Fake Name 8",
+        description: "fake description 8 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        price: "200",
+      },
+      { //spotId 9
+        ownerId: 3,
+        address: "5555 Fake Address Lane",
+        city: "Fake City 9",
+        state: "FakeState9",
+        country: "United States",
+        lat: 29.63933,
+        lng: -5.06108,
+        name: "Fake Name 9",
+        description: "fake description 9 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        price: "210",
+      }, 
     ], { validate: true });
   },
 
